5441b71a34c61649478be7e8df35a846a2fed844
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-04-07 07:42:21 +0530

    Update echo.py

--- JerrySpam/plugins/echo.py
+++ JerrySpam/plugins/echo.py
@@ -6,7 +6,7 @@ import requests
 from telethon import events
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
-from RiZoeLXSpam import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, SUDO_USERS, OWNER_ID
+from JerrySpam import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, SUDO_USERS, OWNER_ID
 
 from RiZoeLXSpam import CMD_HNDLR as hl
 from RiZoeLXSpam.sql.echo_sql import addecho, get_all_echos, is_echo, remove_echo
