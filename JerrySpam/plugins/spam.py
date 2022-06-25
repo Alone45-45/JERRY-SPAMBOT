474106fb8aca603db2049e50287cd2a62a2497bb
Alone45-45 <85700450+Alone45-45@users.noreply.github.com>
2022-06-25 18:52:16 +0530

    Update spam.py

--- JerrySpam/plugins/spam.py
+++ JerrySpam/plugins/spam.py
@@ -19,8 +19,8 @@ import os
 from telethon import events
 from telethon import functions, types
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
-from JerrySpam import Riz, Riz2, Riz3, Riz4, Riz5 , Riz6, Riz7, Riz8, Riz9, Riz10, SUDO_USERS
-from RiZoeLXSpam import CMD_HNDLR as hl
+from JerrySpam import Jerry, Jerry2, Jerry3, Jerry4, Jerry5 , Jerry6, Jerry7, Jerry8, Jerry9, Jerry10, SUDO_USERS
+from JerrySpam import CMD_HNDLR as hl
 from telethon.tl.functions.messages import GetStickerSetRequest
 from telethon.tl.types import InputStickerSetID, InputStickerSetShortName
 from telethon import utils
@@ -28,32 +28,32 @@ from resources.data import GROUP, PORMS
 
 
 
-@Riz.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
-@Riz2.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
-@Riz3.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
-@Riz4.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
-@Riz5.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
-@Riz6.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
-@Riz7.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
-@Riz8.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
-@Riz9.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
-@Riz10.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry2.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry3.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry4.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry5.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry6.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry7.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry8.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry9.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
+@Jerry10.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
 async def spam(e):
     usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗦𝗽𝗮𝗺\n\nCommand:\n\n{hl}spam <count> <message to spam>\n\n{hl}spam <count> <reply to a message>\n\nCount must be a integer."
     error = "Spam Module can only be used till 100 count. For bigger spams use BigSpam."
     if e.sender_id in SUDO_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
-        Rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        JerryT = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         smex = await e.get_reply_message()
-        if len(Rizoel) == 2:
-            message = str(Rizoel[1])
-            counter = int(Rizoel[0])
+        if len(JerryT) == 2:
+            message = str(JerryT[1])
+            counter = int(JerryT[0])
             if counter > 100:
                 return await e.reply(error, parse_mode=None, link_preview=None)
             await asyncio.wait([e.respond(message) for i in range(counter)])
         elif e.reply_to_msg_id and smex.media:
-            counter = int(Rizoel[0])
+            counter = int(JerryT[0])
             if counter > 100:
                 return await e.reply(error, parse_mode=None, link_preview=None)
             for _ in range(counter):
@@ -69,24 +69,24 @@ async def spam(e):
             await e.reply(usage, parse_mode=None, link_preview=None)
 
 
-@Riz.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
-@Riz2.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
-@Riz3.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
-@Riz4.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
-@Riz5.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
-@Riz6.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
-@Riz7.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
-@Riz8.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
-@Riz9.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
-@Riz10.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry2.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry3.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry4.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry5.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry6.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry7.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry8.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry9.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
+@Jerry10.on(events.NewMessage(incoming=True, pattern=r"\%sbigspam" % hl))
 async def spam(e):
     usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗕𝗶𝗴𝗦𝗽𝗮𝗺\n\nCommand:\n\n{hl}bigspam <count> <message to spam>\n\n{hl}bigspam <count> <reply to a message>\n\nCount must be a integer."
     if e.sender_id in SUDO_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None )
-        rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        JerryT = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         smex = await e.get_reply_message()
-        if len(rizoel) == 2:
+        if len(JerryT) == 2:
             message = str(rizoel[1])
             counter = int(rizoel[0])
             for _ in range(counter):
@@ -114,28 +114,28 @@ async def spam(e):
             await e.reply(usage, parse_mode=None, link_preview=None )
 
 
-@Riz.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
-@Riz2.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
-@Riz3.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
-@Riz4.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
-@Riz5.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
-@Riz6.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
-@Riz7.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
-@Riz8.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
-@Riz9.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
-@Riz10.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry2.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry3.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry4.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry5.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry6.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry7.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry8.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry9.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
+@Jerry10.on(events.NewMessage(incoming=True, pattern=r"\%sdelayspam" % hl))
 async def spam(e):
     usage = f"𝗠𝗼𝗱𝘂𝗹𝗲 𝗡𝗮𝗺𝗲 = 𝗗𝗲𝗹𝗮𝘆𝗦𝗽𝗮𝗺\n\nCommand:\n\n{hl}delayspam <sleep time> <count> <message to spam>\n\n{hl}delayspam <sleep time> <count> <reply to a message>\n\nCount and Sleeptime must be a integer."     
     if e.sender_id in SUDO_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None)
         smex = await e.get_reply_message()
-        Rizoel = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
-        Rizoelsexy = Rizoel[1:]
-        if len(Rizoelsexy) == 2:
-            message = str(Rizoelsexy[1])
-            counter = int(Rizoelsexy[0])
-            sleeptime = float(Rizoel[0])
+        JerryT = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+        JerryT = Jerry[1:]
+        if len(JerryT) == 2:
+            message = str(JerryT[1])
+            counter = int(JerryT[0])
+            sleeptime = float(JerryT[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     if e.reply_to_msg_id:
@@ -144,8 +144,8 @@ async def spam(e):
                         await e.client.send_message(e.chat_id, message)
                     await asyncio.sleep(sleeptime)
         elif e.reply_to_msg_id and smex.media:
-            counter = int(Rizoelsexy[0])
-            sleeptime = float(Rizoel[0])
+            counter = int(JerryT[0])
+            sleeptime = float(Jerry[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "document"):
                     smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
@@ -153,8 +153,8 @@ async def spam(e):
                 await asyncio.sleep(sleeptime)
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
-            counter = int(Rizoelsexy[0])
-            sleeptime = float(Rizoel[0])
+            counter = int(JerryT[0])
+            sleeptime = float(Jerry[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     await e.client.send_message(e.chat_id, message)
@@ -163,23 +163,23 @@ async def spam(e):
             await e.reply(usage, parse_mode=None, link_preview=None)
 
 
-@Riz.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
-@Riz2.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
-@Riz3.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
-@Riz4.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
-@Riz5.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
-@Riz6.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
-@Riz7.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
-@Riz8.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
-@Riz9.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
-@Riz10.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry2.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry3.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry4.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry5.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry6.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry7.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry8.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry9.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
+@Jerry0.on(events.NewMessage(incoming=True, pattern=r"\%spornspam(?: |$)(.*)" % hl))
 async def pspam(e):
     if e.sender_id in SUDO_USERS:
         if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
             return await e.reply(usage, parse_mode=None, link_preview=None )
         rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        if len(rizoel) == 1:
-            counter = int(rizoel[0])
+        if len(JerryT) == 1:
+            counter = int(JerryT[0])
             if int(e.chat_id) in GROUP:
                 text = f"Sorry !! I can't spam here"
                 await e.reply(text, parse_mode=None, link_preview=None )
@@ -194,22 +194,22 @@ async def pspam(e):
             usage = f"**MODULE NAME : PORN SPAM** \n\n command: `{hl}pornspam <count>`"
             await e.reply(usage, parse_mode=None, link_preview=None )
 
-@Riz.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
-@Riz2.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
-@Riz3.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
-@Riz4.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
-@Riz5.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
-@Riz6.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
-@Riz7.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
-@Riz8.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
-@Riz9.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
-@Riz10.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry2.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry3.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry4.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry5.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry6.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry7.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry8.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry9.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
+@Jerry10.on(events.NewMessage(incoming=True, pattern=r"\%shang(?: |$)(.*)" % hl))
 async def hang(e):
     usage = f"**MODULE NAME : HANG SPAM** \n\n Cmd : `{hl}hang <count>`"
     if e.sender_id in SUDO_USERS:
-        RiZoeL = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        if len(RiZoeL) == 1:
-            counter = int(RiZoeL[0])
+        JerryT = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(JerryT) == 1:
+            counter = int(JerryT[0])
             if int(e.chat_id) in GROUP:
                 text = f"Sorry !! I can't spam here"
                 await e.reply(text, parse_mode=None, link_preview=None )
@@ -219,16 +219,16 @@ async def hang(e):
         else:
             await e.reply(usage)
 
-@Riz.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
-@Riz2.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
-@Riz3.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
-@Riz4.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
-@Riz5.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
-@Riz6.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
-@Riz7.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
-@Riz8.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
-@Riz9.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
-@Riz10.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry2.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry3.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry4.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry5.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry6.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry7.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry8.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry9.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
+@Jerry10.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
 async def packspam(e):
     if e.sender_id in SUDO_USERS:
         try:
